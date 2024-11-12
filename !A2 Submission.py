a7e38b25e7a7870a3322b7201eee67caf10b7999
BenHeal3y <benjamin.healey@mail.bcu.ac.uk>
2024-11-12 14:39:56 +0000

    Update !A2 Submission.py

--- !A2 Submission.py
+++ !A2 Submission.py
@@ -20,42 +20,15 @@ def A1_ssh(ip_address, username, password_ssh, password_enable):
     else:
         print('Successfully created the session!')
 
-
-
     session.sendline(password_ssh)
-    result = session.expect(['>', pexpect.TIMEOUT, pexpect.EOF])
+    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
-#Check if ">" was actually received   (we have entered the console)
+#Check if "#" was actually received   (we have entered the console)
     if result != 0:
         print('--- Failed to enter password: ', password_ssh)
         exit()
     else:
         print('Successfully entered password!')
-
-
-
-    session.sendline('enable')
-    result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
-
-#Check if (enable) "Password:" was actually received   (enable was entered)
-    if result != 0:
-        print('--- Failed to enter enable mode')
-        exit()
-
-
-
-# Send enable password details
-    session.sendline(password_enable)
-    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
-
-#Check if "#" was actually received   (we entered enable mode)
-    if result != 0:
-        print('--- Failed to enter enable mode after sending password')
-        exit()
-    else:
-        print('Successfully entered enable mode!')
-
-
 
 # Enter configuration mode
     session.sendline('configure terminal')
@@ -230,39 +203,21 @@ def hardening_checks(ip_address,username,password_ssh,password_enable):
     'NTP configured': 'ntp server'
     }
 
-    if result != 0:
+   if result != 0:
         print('--- Failed to create session for: ', ip_address)
         exit()
     else:
         print('Successfully created the session!')
 
     session.sendline(password_ssh)
-    result = session.expect(['>', pexpect.TIMEOUT, pexpect.EOF])
+    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
+#Check if "#" was actually received   (we have entered the console)
     if result != 0:
         print('--- Failed to enter password: ', password_ssh)
         exit()
     else:
         print('Successfully entered password!')
-
-    session.sendline('enable')
-    result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
-
-    if result != 0:
-        print('--- Failed to enter enable mode')
-        exit()
-
-    # Send enable password details
-    session.sendline(password_enable)
-    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
-
-    if result != 0:
-        print('--- Failed to enter enable mode after sending password')
-        exit()
-    else:
-        print('Successfully entered enable mode!')
-    
-    running_config = session.sendline('show running-config')
     
     for check, rule in hardening_items.items():
         if rule in running_config:
@@ -277,37 +232,21 @@ def enable_syslog(username,password_ssh,password_enable,ip_address):
     session = pexpect.spawn(f'ssh {username}@{ip_address}', encoding='utf-8', timeout=20)
     result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
 
-    if result != 0:
+   if result != 0:
         print('--- Failed to create session for: ', ip_address)
         exit()
     else:
         print('Successfully created the session!')
 
     session.sendline(password_ssh)
-    result = session.expect(['>', pexpect.TIMEOUT, pexpect.EOF])
+    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
+#Check if "#" was actually received   (we have entered the console)
     if result != 0:
         print('--- Failed to enter password: ', password_ssh)
         exit()
     else:
-        print('Successfully entered password!')
-
-    session.sendline('enable')
-    result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
-
-    if result != 0:
-        print('--- Failed to enter enable mode')
-        exit()
-
-    # Send enable password details
-    session.sendline(password_enable)
-    result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
-
-    if result != 0:
-        print('--- Failed to enter enable mode after sending password')
-        exit()
-    else:
-        print('Successfully entered enable mode!')
+        print('Successfully entered password!'))
     
     session.sendline("access-list 100 pemit ip 192.168.56.101 0.0.0.255 any")
     session.sendline("access-list deny ip any any ")
